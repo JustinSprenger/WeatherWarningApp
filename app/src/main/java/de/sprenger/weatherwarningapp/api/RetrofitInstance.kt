e3a2bc74ac169ab2b366566993033dd7aaef5f89
Justin Sprenger <justin_sprenger@hotmail.de>
2024-08-14 02:36:15 +0200

    Add Retrofit Instance

---
+++ app/src/main/java/de/sprenger/weatherwarningapp/api/RetrofitInstance.kt
@@ -0,0 +1,14 @@
+package de.sprenger.weatherwarningapp.api
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitInstance {
+    private const val BASE_URL = "https://warnung.bund.de/"
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    val warningService: GetWarningService = retrofit.create(GetWarningService::class.java)
+}
