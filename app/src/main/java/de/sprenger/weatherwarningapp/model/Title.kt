41052f45fb6b05e1be3345855f8a0ea8c1036481
Justin Sprenger <justin_sprenger@hotmail.de>
2024-08-14 04:21:08 +0200

    switch some data var to nullable

--- app/src/main/java/de/sprenger/weatherwarningapp/model/Title.kt
+++ app/src/main/java/de/sprenger/weatherwarningapp/model/Title.kt
@@ -2,11 +2,11 @@ package de.sprenger.weatherwarningapp.model
 
 data class Title(
     val de: String?,
-    val en: String,
-    val ar: String,
-    val es: String,
-    val fr: String,
-    val pl: String,
-    val ru: String,
-    val tr: String
+    val en: String?,
+    val ar: String?,
+    val es: String?,
+    val fr: String?,
+    val pl: String?,
+    val ru: String?,
+    val tr: String?
 )
